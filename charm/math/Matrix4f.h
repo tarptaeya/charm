48669a35a0b82b01f170d58123d5ee6c177a78fb
Anmol <tarptaeya@gmail.com>
2025-07-31 23:17:51 -0700

    added more comments

--- charm/math/Matrix4f.h
+++ charm/math/Matrix4f.h
@@ -5,13 +5,16 @@ namespace charm {
 /**
  * This class represents a 4x4 matrix of floats. The
  * matrix works like a row major matrix, but is stored
- * internally as a column major order.
+ * internally in a column major order.
  */
 class Matrix4f {
     float m_data[16] = { 0 };
 
 public:
     Matrix4f() = default;
+    /**
+     * Constructs Matrix4f from column major float[16] array
+     */
     Matrix4f(const float (&)[16]);
     ~Matrix4f() = default;
 
@@ -21,6 +24,9 @@ public:
     Matrix4f(Matrix4f&&) = default;
     Matrix4f& operator=(Matrix4f&&) = default;
 
+    /**
+     * Returns the matrix data in column major order.
+     */
     const float* get_data() const;
 
     Matrix4f& operator+=(const Matrix4f&);
