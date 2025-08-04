1ae37a47da0bdb54c3781e8bd8bc4af3fe793538
Anmol <tarptaeya@gmail.com>
2025-08-03 22:36:15 -0700

    add more boxes in demo

--- src/demo/main.cpp
+++ src/demo/main.cpp
@@ -18,16 +18,33 @@ public:
         XProgram program = std::move(XProgram(xShaders.get("basic.vertex"), xShaders.get("basic.fragment")));
         xMaterials.add("basic", XMaterial(std::move(program)));
 
+        float positions[10][3] = {
+            { 0, 0, 0 },
+            { 2, 5, -15 },
+            { -1.5, -2.2, -2.5 },
+            { -3.8, -2.0, -12.3 },
+            { 2.4, -0.4, -3.5 },
+            { -1.7, 3.0, -7.5 },
+            { 1.3, -2.0, -2.5 },
+            { 1.5, 2.0, -2.5 },
+            { 1.5, 0.2, -1.5 },
+            { -1.3, 1.0, -1.5 }
+        };
+
         for (int i = 0; i < 10; ++i) {
             XEntity entity;
             entity.add_component<XMeshRendererComponent>(m_box_geometry, xMaterials.get("basic"));
+            XMatrix4f transform = XMatrix4f::identity();
+            transform *= XMatrix4f::translation(positions[i][0], positions[i][1], positions[i][2]);
+            transform *= XMatrix4f::rotation_x(i * 20) * XMatrix4f::rotation_y(i * 10) * XMatrix4f::rotation_z(i * 30);
+            entity.get_component<XTransformComponent>()->set_transform(transform);
             m_entities.push_back(std::move(entity));
         }
 
         m_texture = XTexture2DBuilder("assets/container.ppm").set_texture_unit(GL_TEXTURE0).build();
 
         m_camera.set_projection(XMatrix4f::perspective(M_PI_4, 1024.0 / 720.0, 0.1, 100));
-        m_camera.set_transform(XMatrix4f::identity() * XMatrix4f::translation(0, 0, 10));
+        m_camera.set_transform(XMatrix4f::identity() * XMatrix4f::translation(0, 0, 3));
     }
 
     ~GameAdapter() override = default;
@@ -35,19 +52,13 @@ public:
     void update(double delta_time) override
     {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glClearColor(0.1, 0.2, 0.3, 1.0);
+        glClearColor(0.5, 0.6, 0.7, 1.0);
 
         static float theta = 0;
         theta += delta_time;
 
         int count = 0;
         for (XEntity& entity : m_entities) {
-            ++count;
-            XMatrix4f transform = XMatrix4f::identity();
-            transform *= XMatrix4f::translation(count - 5, count - 3, -2 * count);
-            theta *= -1;
-            transform *= XMatrix4f::rotation_x(theta) * XMatrix4f::rotation_z(theta);
-            entity.get_component<XTransformComponent>()->set_transform(transform);
             m_renderer.render(entity, m_camera);
         }
     }
