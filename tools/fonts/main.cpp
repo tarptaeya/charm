6d1ed44ee5460a40fe0c6c80dd187d9556df11fe
Anmol <tarptaeya@gmail.com>
2025-08-14 22:52:43 -0700

    emit metadata for font bitmap

--- tools/fonts/main.cpp
+++ tools/fonts/main.cpp
@@ -3,22 +3,40 @@
 
 #include "stb_image_write.h"
 #include "stb_truetype.h"
+#include <fstream>
 #include <iostream>
 
 #define width 512
 #define height 512
+#define pixel_height 64
 
-unsigned char buffer[1 << 20];
+unsigned char buffer[1 << 25];
 unsigned char bitmap[width * height];
-stbtt_bakedchar cdata[96];
+stbtt_bakedchar cdata[95];
 
 int main(int argc, const char** argv)
 {
-    if (argc != 3) {
-        std::cout << "[usage] font-conv path-to-font.ttf output-name" << std::endl;
+    if (argc != 2) {
+        std::cout << "[usage] font-conv path-to-font.ttf" << std::endl;
         std::exit(0);
     }
-    fread(buffer, 1, 1 << 20, fopen(argv[1], "rb"));
-    stbtt_BakeFontBitmap(buffer, 0, 32, bitmap, width, height, 32, 96, cdata);
-    stbi_write_png(argv[2], width, height, 1, bitmap, width);
+    fread(buffer, 1, 1 << 25, fopen(argv[1], "rb"));
+
+    int start_char = 32;
+    int num_char = 95;
+    stbtt_BakeFontBitmap(buffer, 0, pixel_height, bitmap, width, height, start_char, num_char, cdata);
+    stbi_write_png("bitmap.png", width, height, 1, bitmap, width);
+
+    std::ofstream f("font.txt");
+    for (int i = 0; i < num_char; ++i) {
+        char c = start_char + i;
+        f << c << " "
+          << cdata[i].x0 << " "
+          << cdata[i].y0 << " "
+          << cdata[i].x1 << " "
+          << cdata[i].y1 << " "
+          << cdata[i].xoff << " "
+          << cdata[i].yoff << " "
+          << cdata[i].xadvance << std::endl;
+    }
 }
