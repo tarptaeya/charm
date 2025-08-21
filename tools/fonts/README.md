9cf212a32697145fdaff779349fd67ffc226682b
Anmol <tarptaeya@gmail.com>
2025-08-20 19:24:48 -0700

    updated font-conv to produce sdf bitmaps

--- tools/fonts/README.md
+++ tools/fonts/README.md
@@ -3,8 +3,8 @@
 Converts ttf font to bitmap file and font metadata (bitmap.png + font.txt)
 
 Font metadata contains information about position of each character in the bitmap. First line
-of the metadata file (font.txt) contains width and height of the bitmap font, after that each
-line consists of space separated char, x, y, width, height, xoffset, yoffset, and xadvance.
+of the metadata file (font.txt) contains width, height and pixel height (font size) of the bitmap font, 
+after that each line consists of space separated char, x, y, width, height, xoffset, yoffset, and xadvance.
 
 yoffset is the distance of the top of character to the base line of the char (with y axis pointing downwards).
 
--- tools/fonts/main.cpp
+++ tools/fonts/main.cpp
@@ -1,43 +1,111 @@
-#define STB_TRUETYPE_IMPLEMENTATION
-#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <ft2build.h>
+#include FT_FREETYPE_H
+#include FT_GLYPH_H
 
-#include "stb_image_write.h"
-#include "stb_truetype.h"
 #include <fstream>
 #include <iostream>
+#include <string>
 
-#define width 512
-#define height 512
-#define pixel_height 64
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
 
-unsigned char buffer[1 << 25];
-unsigned char bitmap[width * height];
-stbtt_bakedchar cdata[95];
+struct CharacterInfo {
+    char c;
+    int x, y; // Position in texture atlas
+    int width, height; // width, height of bbox in texture atlas
+    int xoffset;
+    int yoffset;
+    float xadvance;
+    unsigned char* bitmap_buffer;
+};
 
 int main(int argc, const char** argv)
 {
-    if (argc != 2) {
-        std::cout << "[usage] font-conv path-to-font.ttf" << std::endl;
-        std::exit(0);
+    FT_Library library;
+    if (FT_Init_FreeType(&library)) {
+        std::cerr << "[error] cannot initialize freetype" << std::endl;
+        std::exit(1);
     }
-    fread(buffer, 1, 1 << 25, fopen(argv[1], "rb"));
 
-    int start_char = 32;
-    int num_char = 95;
-    stbtt_BakeFontBitmap(buffer, 0, pixel_height, bitmap, width, height, start_char, num_char, cdata);
-    stbi_write_png("bitmap.png", width, height, 1, bitmap, width);
+    FT_Face face;
+    if (FT_New_Face(library, argv[1], 0, &face)) {
+        std::cerr << "[error] cannot load font" << std::endl;
+        std::exit(1);
+    }
 
+    int font_pixel_height = 64;
+    FT_Set_Pixel_Sizes(face, 0, font_pixel_height);
+
+    std::vector<CharacterInfo> all_info;
+    for (char c = 32; c < 127; ++c) {
+        FT_Load_Char(face, c, FT_LOAD_RENDER);
+        FT_Render_Glyph(face->glyph, FT_RENDER_MODE_SDF);
+
+        if (face->glyph->bitmap.width != face->glyph->bitmap.pitch) {
+            std::cerr << "[error] bitmap width != bitmap pitch for [" << c << "]" << std::endl;
+            std::exit(1);
+        }
+
+        CharacterInfo info;
+        info.c = c;
+        info.width = face->glyph->bitmap.width;
+        info.height = face->glyph->bitmap.rows;
+        info.xoffset = face->glyph->bitmap_left;
+        info.yoffset = -face->glyph->bitmap_top;
+        info.xadvance = face->glyph->advance.x / 64.0; // Freetype metrics are expressed in 26.6 pixel format (i.e. 1/64th of pixel)
+        info.bitmap_buffer = static_cast<unsigned char*>(malloc(info.width * info.height));
+        std::memcpy(info.bitmap_buffer, face->glyph->bitmap.buffer, info.width * info.height);
+        all_info.push_back(info);
+    }
+
+    int xcurr = 0;
+    int line_height = 0;
+    int num_rows = 1;
+    int max_allowed_width = 1024;
+    for (int i = 0; i < all_info.size(); ++i) {
+        if (xcurr + all_info[i].width <= max_allowed_width) {
+            xcurr += all_info[i].width;
+        } else {
+            num_rows += 1;
+            xcurr = all_info[i].width;
+        }
+        line_height = std::max(all_info[i].height, line_height);
+    }
+
+    int total_width = max_allowed_width;
+    int total_height = num_rows * line_height;
+
+    unsigned char* atlas_buffer = static_cast<unsigned char*>(malloc(total_width * total_height));
+    std::memset(atlas_buffer, 0, total_width * total_height);
+    int xcursor = 0;
+    int ycursor = 0;
+    for (auto& info : all_info) {
+        if (xcursor + info.width > total_width) {
+            xcursor = 0;
+            ycursor += line_height;
+        }
+
+        info.x = xcursor;
+        info.y = ycursor;
+        for (int y = 0; y < info.height; ++y) {
+            for (int x = 0; x < info.width; ++x) {
+                atlas_buffer[(y + ycursor) * total_width + (x + xcursor)] = info.bitmap_buffer[y * info.width + x];
+            }
+        }
+        xcursor += info.width;
+    }
+
+    stbi_write_png("bitmap.png", total_width, total_height, 1, atlas_buffer, total_width);
     std::ofstream f("font.txt");
-    f << width << " " << height << " " << pixel_height << std::endl;
-    for (int i = 0; i < num_char; ++i) {
-        char c = start_char + i;
-        f << c << " "
-          << cdata[i].x0 << " "
-          << cdata[i].y0 << " "
-          << cdata[i].x1 << " "
-          << cdata[i].y1 << " "
-          << cdata[i].xoff << " "
-          << cdata[i].yoff << " "
-          << cdata[i].xadvance << std::endl;
+    f << total_width << " " << total_height << " " << font_pixel_height << std::endl;
+    for (const auto& info : all_info) {
+        f << info.c << " "
+          << info.x << " "
+          << info.y << " "
+          << info.width << " "
+          << info.height << " "
+          << info.xoffset << " "
+          << info.yoffset << " "
+          << info.xadvance << std::endl;
     }
 }
